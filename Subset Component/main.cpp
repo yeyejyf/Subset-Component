b697fcc73162017050613c928431c83984f4ac08
yeyejyf <yunfan.jin@gmail.com>
2015-03-16 21:49:05 -0400

    Initial Commit

---
+++ Subset Component/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Subset Component
+//
+//  Created by Yunfan Jin on 3/16/15.
+//  Copyright (c) 2015 yunfan.jin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
