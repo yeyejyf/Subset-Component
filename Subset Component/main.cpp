381378dc28e5e1dd8a7f3c690771e25ab8a914d8
yeyejyf <yunfan.jin@gmail.com>
2015-03-16 21:53:19 -0400

    test

--- Subset Component/main.cpp
+++ Subset Component/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    unsigned long long t;
+    
+    
     return 0;
 }
